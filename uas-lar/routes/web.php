e24f46d0d8e75dbdb6b8142c58f9acca4a1140bd
erixpraditya <erixpradityaa@gmail.com>
2025-06-24 16:02:31 +0700

    logout

--- uas-lar/routes/web.php
+++ uas-lar/routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Auth;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('home');
+})->middleware('auth');
 
 Route::get('/home', function () {
     return view('home');
-});
+})->middleware('auth');
 
 Route::get('/cektemplate', function () {
     return view('layouts.template');
